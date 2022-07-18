41ff9bc2ddc157f61d8bce6ae21e13a4e8ae5b31
Luzhanskiy7 <107493221+Luzhanskiy7@users.noreply.github.com>
2022-07-18 20:59:56 +0300

    visual efect win or lose

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,12 +8,14 @@ tk.geometry('377x367')
 tk.resizable(False, False)
 tk.config(cursor="target")
 MAX_BUTTON = 9
+
 def start():
     global game_field
     global game_remains
     global button
+
     game_field = [None] * MAX_BUTTON
-    game_remains = list(range(9))
+    game_remains = list(range(MAX_BUTTON))
     button = [Button(width=5, height=2, font=('Arial', 28, 'bold'), bg='black',
                   command=lambda x=i: push(x)) for i in range(MAX_BUTTON)]
     row = 0
@@ -29,14 +31,47 @@ start()
 
 def win(n):
     global game_field
-    if (game_field[0] == n and game_field[3] == n and game_field[6] == n) or\
-        (game_field[0] == n and game_field[1] == n and game_field[2] == n) or\
-        (game_field[3] == n and game_field[4] == n and game_field[5] == n) or\
-        (game_field[6] == n and game_field[7] == n and game_field[8] == n) or\
-        (game_field[1] == n and game_field[4] == n and game_field[7] == n) or\
-        (game_field[2] == n and game_field[5] == n and game_field[8] == n) or\
-        (game_field[0] == n and game_field[4] == n and game_field[8] == n) or\
-        (game_field[2] == n and game_field[4] == n and game_field[6] == n):
+    global batton
+
+    if (game_field[0] == n and game_field[3] == n and game_field[6] == n):
+        button[0].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[3].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[6].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[0] == n and game_field[1] == n and game_field[2] == n):
+        button[0].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[1].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[2].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[3] == n and game_field[4] == n and game_field[5] == n):
+        button[3].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[4].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[5].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[6] == n and game_field[7] == n and game_field[8] == n):
+        button[6].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[7].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[8].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[1] == n and game_field[4] == n and game_field[7] == n):
+        button[1].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[4].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[7].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[2] == n and game_field[5] == n and game_field[8] == n):
+        button[2].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[5].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[8].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[0] == n and game_field[4] == n and game_field[8] == n):
+        button[0].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[4].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[8].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        return True
+    elif (game_field[2] == n and game_field[4] == n and game_field[6] == n):
+        button[2].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[4].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
+        button[6].config(text=n, bg='red', fg='black', cursor='pirate', state='disabled')
         return True
 
 def message(choice):
